d69ad5d0a42fd99ca63557bbc89fa95e89a77654
Dane Laban <TheZepto@users.noreply.github.com>
2017-10-11 16:02:52 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -19,9 +19,9 @@ def scrape_page(page)
         "date_scraped" => Date.today.to_s
       }
       if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-        puts "Saving record " + record['council_reference'] + " - " + record['address']
+#         puts "Saving record " + record['council_reference'] + " - " + record['address']
 #         puts record
-#         ScraperWiki.save_sqlite(['council_reference'], record)
+        ScraperWiki.save_sqlite(['council_reference'], record)
 #       else
 #         puts "Skipping already saved record " + record['council_reference']
       end
